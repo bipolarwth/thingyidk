83b59733bc59de1b53efc9fa08ac4b333c720f34
Alexander <alexander.bussian@googlemail.com>
2015-10-15 19:22:00 +0200

    Added simple Login Mask and function

--- app/app.js
+++ app/app.js
@@ -1,12 +1,18 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MainController', ['$scope', function($scope) {
+
+  //$scope.scopeTest = "scopeTest";
+
+  $scope.user = [];
+  $scope.user.name = "username here";
+  $scope.user.password = "password here";
+
+  $scope.login = function() {
+    console.log("login with " + $scope.user.name + " and " + $scope.user.password);
+  };
+
 }]);
